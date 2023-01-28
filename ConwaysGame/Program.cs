85694bca89c3f786c6d5518b64d3df851dd670ca
cordovinian <schmidt.matt@gmail.com>
2023-01-27 17:15:19 -0800

    fix: Indexing issue and print issue

--- ConwaysGame/Program.cs
+++ ConwaysGame/Program.cs
@@ -6,18 +6,19 @@ namespace ConwaysGame
     {
         public static void Main(string[] args)
         {
-            // See https://aka.ms/new-console-template for more information
-            Console.WriteLine("Hello, World!");
-
-            var startGrid = new List<List<bool>>{
+            // TEMP: Start with a known grid
+            var grid = new List<List<bool>>{
                 new List<bool> { false, false, false},
                 new List<bool> { true, true, true},
                 new List<bool> { false, false, false},
             };
-            PrintGrid(startGrid);
-            var finalGrid = Transition(startGrid);
-            PrintGrid(finalGrid);
 
+            PrintGrid(grid);
+
+            foreach(var iteration in Enumerable.Range(1,2))
+            {
+                PrintGrid(Transition(grid));
+            }
         }
 
         public static bool ReturnsTrueAlways() => true;
@@ -25,21 +26,40 @@ namespace ConwaysGame
         public static void PrintGrid(List<List<bool>> grid)
         {
             Console.WriteLine("Grid state:");
+
             foreach(var row in grid)
             {
-                Console.WriteLine(row);
+                var formattedDataRow = string.Empty;
+                var tableRow = string.Empty;
+
+                foreach(var col in row)
+                {
+                    formattedDataRow += String.Format($"{col, -5} | ");
+                    tableRow += (String.Format($"----- | "));
+                }
+
+                // Remove extra row separator before printing
+                formattedDataRow = formattedDataRow.Remove(formattedDataRow.Length - 3, 2);
+                tableRow = tableRow.Remove(tableRow.Length - 3, 2);
+
+                Console.WriteLine(formattedDataRow);
+                Console.WriteLine(tableRow);
             }
         }
+
         public static List<List<bool>> Transition(List<List<bool>> grid)
         {
             var finalGrid = new List<List<bool>>();
 
-            var row = grid.Count();
             // Assume width is same for all rows
+            var row = grid.Count();
             var col = grid[0].Count();
 
             for (int rr = 0; rr < row; rr++)
             {
+                // Initialize new row in final grid
+                finalGrid.Add(new List<bool>(col));
+
                 for (int cc = 0; cc < col; cc++)
                 {
                     var peers = new List<bool>(8);
@@ -81,22 +101,22 @@ namespace ConwaysGame
 
                     if (grid[rr][cc] == true && peerCount < 2)
                     {
-                        finalGrid[rr][cc] = false;
+                        finalGrid[rr].Add(false);
                     }
                     else if (grid[rr][cc] == true && (peerCount == 2 || peerCount == 3))
                     {
-                        finalGrid[rr][cc] = true;
+                        finalGrid[rr].Add(true);
                     }
                     else if (grid[rr][cc] == true && (peerCount >= 3))
                     {
-                        finalGrid[rr][cc] = false;
+                        finalGrid[rr].Add(false);
                     }
                     else if (grid[rr][cc] == false && (peerCount == 3))
                     {
-                        finalGrid[rr][cc] = true;
+                        finalGrid[rr].Add(true);
                     }
                     else {
-                        finalGrid[rr][cc] = false;
+                        finalGrid[rr].Add(false);
                     }
                 }
             }
