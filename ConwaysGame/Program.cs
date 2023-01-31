85ac8f0860df167b3a90a8d40099fbe3dec7ce94
cordovinian <schmidt.matt@gmail.com>
2023-01-30 19:57:08 -0800

    feat: Refactor out Grid object

--- ConwaysGame/Program.cs
+++ ConwaysGame/Program.cs
@@ -8,26 +8,39 @@ namespace ConwaysGame
     /// <summary>The main class for the program.</summary>
     public class ConwaysGameClass
     {
+        private static List<List<int>> oscillatingBlinkerGrid = new List<List<int>>{
+            new List<int> { 0, 1, 0},
+            new List<int> { 0, 1, 0},
+            new List<int> { 0, 1, 0},
+        };
+
         /// <summary>
         /// Plays Conway's game of life for a number of iterations.
         /// </summary>
-        /// <param name="iterations">The number of game iterations to go through.</param>
         /// <param name="input">The name of the input file to set as the starting grid state.</param>
-        /// <param name="inPlace">Enable to refresh grid in-place on console.</param>
-        /// <param name="gridLines">Enable showing grid lines.</param>
+        /// <param name="iterations">The number of game iterations to go through.</param>
         /// <param name="refreshRate">How fast to recalculate next grid.</param>
-        public static void Main(FileInfo input, int iterations = 20, bool inPlace = true, bool gridLines = false, int refreshRate = 100)
+        /// <param name="eachStep">Enable to display each iteration step.</param>
+        /// <param name="gridLines">Enable showing grid lines.</param>
+        /// <param name="boxLines">Enable showing bounding box lines.</param>
+        public static void Main(
+                                FileInfo input,
+                                int iterations = 20,
+                                int refreshRate = 100,
+                                bool eachStep = false,
+                                bool gridLines = false,
+                                bool boxLines = false)
         {
-            var inputGrid = new List<List<int>>();
+            var inputGrid = new Grid();
 
             Console.CursorVisible = false;
 
             if (input == null)
             {
-                inputGrid = oscillatingBlinkerGrid;
+                inputGrid = new Grid(oscillatingBlinkerGrid);
 
                 Console.WriteLine("Default Grid - 'Blinker'");
-                WriteGrid(inputGrid, inPlace: false, gridLines, "Default State:");
+                WriteGrid(inputGrid, "Default State:", eachStep: true, gridLines, boxLines);
             }
             else if (input != null)
             {
@@ -37,118 +50,46 @@ namespace ConwaysGame
                     return;
                 }
 
-                inputGrid = ParseGrid(input.FullName, null);
+                var gridParseResult = Grid.TryParseGrid(out inputGrid, input.FullName, null);
+                if (!gridParseResult || inputGrid == null)
+                {
+                    Console.Error.WriteLine($"Failed to parse the file '{input.Name}' as a valid grid.");
+                    return;
+                }
 
                 Console.WriteLine($"From file - '{input.Name}'");
-                WriteGrid(inputGrid, inPlace: false, gridLines, $"Initial State:");
+                WriteGrid(inputGrid, $"Initial State:", eachStep: true, gridLines, boxLines);
             }
 
             for (var ii = 1; ii <= iterations; ii++)
             {
                 Thread.Sleep(refreshRate);
-                inputGrid = Transition(inputGrid);
-                WriteGrid(inputGrid, inPlace: inPlace, gridLines, $"Step {ii}:");
+                inputGrid.Transition();
+                WriteGrid(inputGrid, $"Step {ii}:", eachStep, gridLines, boxLines);
             }
         }
 
-        private static List<List<int>> oscillatingBlinkerGrid = new List<List<int>>{
-                new List<int> { 0, 1, 0},
-                new List<int> { 0, 1, 0},
-                new List<int> { 0, 1, 0},
-            };
-
-        // Parses a text file as a grid.
-        private static List<List<int>> ParseGrid(string inputFileInfo, char[]? delimeters)
+        private static void WriteGrid(
+                                        Grid grid,
+                                        string header = "Grid State:",
+                                        bool eachStep = false,
+                                        bool gridLines = false,
+                                        bool boxLines = false)
         {
-            delimeters = delimeters ?? new char[] { ',', '\t', ';' };
+            var gridOutput = grid.ToString(gridLines, boxLines).Split('\n');
 
-            var grid = new List<List<int>>();
+            var lineLength = gridOutput[0].Length;
 
-            int columnCount = 0;
-
-            foreach (var line in System.IO.File.ReadLines(inputFileInfo))
-            {
-                var splitLine = line.Split(delimeters, StringSplitOptions.TrimEntries);
-
-                if (splitLine.Length <= 0)
-                {
-                    throw new Exception("Parsed values don't match the expected number of cells in the row");
-                }
-
-                columnCount = columnCount == 0 ? splitLine.Length : columnCount;
-
-                var row = new List<int>();
-
-                foreach (var cell in splitLine)
-                {
-                    int cellValue = 0;
-                    int.TryParse(cell, out cellValue);
-                    row.Add(cellValue);
-                }
-
-                if (row.Count != columnCount)
-                {
-                    throw new Exception("Parsed values don't match the expected number of cells in the row");
-                }
-
-                grid.Add(row);
-            }
-
-            return grid;
-        }
-
-        // Format the Grid type into a console output grid
-        private static string FormatGrid(List<List<int>> grid, bool gridLines = false)
-        {
-            string output = string.Empty;
-
-            foreach (var row in grid)
-            {
-                var formattedDataRow = string.Empty;
-                var tableRow = string.Empty;
-
-                foreach (var col in row)
-                {
-                    var cell = col == 1 ? '■' : ' ';
-                    var separator = gridLines ? " | " : " ";
-
-                    formattedDataRow += String.Format($"{cell,-1}{separator}");
-                }
-
-
-                if (gridLines)
-                {
-                    // NOTE: Remove extra row separator before printing
-                    formattedDataRow = formattedDataRow.Remove(formattedDataRow.Length - 3, 2);
-                    output += string.Concat(formattedDataRow, '\n');
-
-                    tableRow = new StringBuilder().Insert(0, "--|-", row.Count).ToString();
-                    tableRow = tableRow.Remove(tableRow.Length - 3, 2);
-                    output += string.Concat(tableRow, '\n');
-                }
-                else
-                {
-                    output += string.Concat(formattedDataRow, '\n');
-                }
-            }
-
-            return output;
-        }
-
-        private static void WriteGrid(List<List<int>> grid, bool inPlace = true, bool gridLines = false, string header = "Grid State:")
-        {
-            var output = FormatGrid(grid, gridLines).Split('\n');//, StringSplitOptions.RemoveEmptyEntries);
-
-            if (inPlace)
+            if (!eachStep)
             {
                 // NOTE: Shift console up rows (value + separator) plus header plus space
-                int newCursorRow = (Console.CursorTop - output.Length - 1) < 0 ? 0 : Console.CursorTop - output.Length - 1;
+                int newCursorRow = (Console.CursorTop - gridOutput.Length - 1) < 0 ? 0 : Console.CursorTop - gridOutput.Length - 1;
                 Console.SetCursorPosition(0, newCursorRow);
 
                 // Clear grid space
-                for (int i = 0; i < output.Length + 1; i++)
+                for (int i = 0; i < gridOutput.Length + 1; i++)
                 {
-                    var clearLineLength = i == 0 ? Console.WindowWidth : output[0].Length;
+                    var clearLineLength = i == 0 ? Console.WindowWidth : lineLength;
                     Console.WriteLine(new string(' ', clearLineLength));
                 }
 
@@ -160,90 +101,11 @@ namespace ConwaysGame
                 Console.WriteLine(header);
             }
 
-            foreach (var line in output)
+            // Grid
+            foreach (var line in gridOutput)
             {
                 Console.WriteLine(line);
             }
         }
-
-        /// Method to analyze the current state of the game grid and transition to the next state.
-        protected static List<List<int>> Transition(List<List<int>> grid)
-        {
-            var finalGrid = new List<List<int>>();
-
-            // ASSUME: width is same for all rows
-            var row = grid.Count();
-            var col = grid[0].Count();
-
-            for (int rr = 0; rr < row; rr++)
-            {
-                // Initialize new row in final grid
-                finalGrid.Add(new List<int>(col));
-
-                for (int cc = 0; cc < col; cc++)
-                {
-                    var peers = new List<int>(8);
-
-                    if (rr - 1 >= 0)
-                    {
-                        if (cc - 1 >= 0)
-                        {
-                            peers.Add(grid[rr - 1][cc - 1]);
-                        }
-                        peers.Add(grid[rr - 1][cc]);
-                        if (cc + 1 < col)
-                            peers.Add(grid[rr - 1][cc + 1]);
-                    }
-
-                    if (cc - 1 >= 0)
-                    {
-                        peers.Add(grid[rr][cc - 1]);
-                    }
-                    if (cc + 1 < col)
-                    {
-                        peers.Add(grid[rr][cc + 1]);
-                    }
-
-                    if (rr + 1 < row)
-                    {
-                        if (cc - 1 >= 0)
-                        {
-                            peers.Add(grid[rr + 1][cc - 1]);
-                        }
-                        peers.Add(grid[rr + 1][cc]);
-                        if (cc + 1 < col)
-                        {
-                            peers.Add(grid[rr + 1][cc + 1]);
-                        }
-                    }
-
-                    var peerCount = peers.Count(x => x == 1);
-
-                    if (grid[rr][cc] == 1 && peerCount < 2)
-                    {
-                        finalGrid[rr].Add(0);
-                    }
-                    else if (grid[rr][cc] == 1 && (peerCount == 2 || peerCount == 3))
-                    {
-                        finalGrid[rr].Add(1);
-                    }
-                    else if (grid[rr][cc] == 1 && (peerCount >= 3))
-                    {
-                        finalGrid[rr].Add(0);
-                    }
-                    else if (grid[rr][cc] == 0 && (peerCount == 3))
-                    {
-                        finalGrid[rr].Add(1);
-                    }
-                    else
-                    {
-                        finalGrid[rr].Add(0);
-                    }
-                }
-            }
-
-            return finalGrid;
-        }
-
     }
 }
--- ConwaysGameTests/UnitTests.cs
+++ ConwaysGameTests/UnitTests.cs
@@ -5,6 +5,7 @@ namespace ConwaysGameTests;
 
 public class ConwaysGameTests : ConwaysGameClass
 {
+    /*
     [Fact]
     public void Transition_ReturnsCorrectGrid()
     {
@@ -25,6 +26,104 @@ public class ConwaysGameTests : ConwaysGameClass
         var endGrid = ConwaysGameClass.Transition(startGrid);
 
         // Then
+        endGrid.Should().BeEquivalentTo(expectedGrid);
+    }
+    */
+}
+
+public class GridTests : Grid
+{
+    [Fact]
+    public void Grid_ReturnsEmptyGrid()
+    {
+        // Given & when
+        var newGrid = new Grid();
+        var gridAs2DList = newGrid.As2DList();
+
+        // Then
+        newGrid.Width.Should().Be(0);
+        newGrid.Length.Should().Be(0);
+        gridAs2DList.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Grid_ReturnsGridMatchingInput()
+    {
+        // Given
+        var expectedGrid = new List<List<int>>{
+            new List<int>{ 0, 0, 0 },
+            new List<int>{ 1, 1, 1 },
+            new List<int>{ 0, 0, 0 }
+        };
+
+        // When
+        var actualGrid = new Grid(expectedGrid);
+        var actualAs2DList = actualGrid.As2DList();
+
+        // Then
+        actualGrid.Width.Should().Be(3);
+        actualGrid.Length.Should().Be(3);
+        actualAs2DList.Should().BeEquivalentTo(expectedGrid);
+    }
+
+
+    [Fact]
+    public void TryParseGrid_ReturnsGridAndSucess()
+    {
+        // Given
+        var actualGrid = new Grid();
+        var file = new FileInfo("GridSamples/Oscillator-Blinker.txt");
+        var expectedGrid = new List<List<int>>{
+            new List<int>{ 0, 0, 0 },
+            new List<int>{ 1, 1, 1 },
+            new List<int>{ 0, 0, 0 }
+        };
+
+        // When
+        var result = Grid.TryParseGrid(out actualGrid, file.FullName, null);
+        int expectedWidth = 0, expectedLength = 0;
+
+        foreach (var line in System.IO.File.ReadLines(file.FullName))
+        {
+            if (expectedLength <= 0)
+            {
+                var cells = line.Split(',', StringSplitOptions.RemoveEmptyEntries & StringSplitOptions.TrimEntries);
+                expectedWidth = cells.Length;
+            }
+            expectedLength++;
+        }
+        var actualAs2DList = actualGrid?.As2DList();
+
+        // Then
+        result.Should().BeTrue("TryParseGrid did not return success.");
+        actualGrid.Should().NotBeNull();
+        actualGrid?.Width.Should().Be(expectedWidth);
+        actualGrid?.Length.Should().Be(expectedLength);
+        actualAs2DList.Should().BeEquivalentTo(expectedGrid);
+    }
+
+    [Fact]
+    public void Transition_UpdatesGridCorrectly()
+    {
+        // given
+        var startGrid = new List<List<int>>{
+            new List<int> { 0, 0, 0},
+            new List<int> { 1, 1, 1},
+            new List<int> { 0, 0, 0},
+        };
+        var grid = new Grid(startGrid);
+
+        var expectedGrid = new List<List<int>>{
+            new List<int> { 0, 1, 0},
+            new List<int> { 0, 1, 0},
+            new List<int> { 0, 1, 0},
+        };
+
+        // When
+        grid.Transition();
+
+        // Then
+        var endGrid = grid.As2DList();
         endGrid.Should().BeEquivalentTo(expectedGrid);
     }
 }
